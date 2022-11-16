2507530770f7028a46660649b47789aefe66acee
liujian <liujianai@digitialchina.com>
2022-11-16 15:14:20 +0800

    添加修改配置操作

--- src/config/main.go
+++ src/config/main.go
@@ -1,11 +1,10 @@
-package config
+package main
 
 import (
 	_ "embed"
 	"encoding/base64"
-	"encoding/json"
-	log "go-micro.dev/v4/logger"
-	"net/http"
+	"github.com/gin-gonic/gin"
+	"log"
 )
 
 //go:embed public_key.pem
@@ -14,26 +13,44 @@ var publicKey []byte
 //go:embed private_key.pem
 var privateKey []byte
 
-func HttpConfig() {
+func main() {
+	router := gin.Default()
+	config := defaultConfig()
+	router.POST("config", func(c *gin.Context) {
+		err := c.Bind(config)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"code": -1,
+				"msg":  "failed to get new config",
+			})
+			c.Abort()
+		}
+		c.JSON(200, gin.H{
+			"code": 0,
+			"msg":  "success",
+		})
+	})
+
+	router.GET("config", func(c *gin.Context) {
+		c.JSON(200, config)
+	})
+
+	err := router.Run(":8080")
+	if err != nil {
+		log.Fatalf("start config failed, the error is %v", err)
+	}
+}
+
+func defaultConfig() *Configuration {
 	c := new(Configuration)
 	c.Auth.WhiteList = []string{
-		"/signIn",
-		"/signUp",
+		"/login",
 	}
 	c.Auth.PublicKey = base64.StdEncoding.EncodeToString(publicKey)
 	c.Auth.PrivateKey = base64.StdEncoding.EncodeToString(privateKey)
 	c.Auth.ExpireTime = 1200
 	c.Auth.RefreshTime = 120
-	handler := http.NewServeMux()
-
-	handler.HandleFunc("/config", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Header().Set("Content-Type", "Application/json")
-		data, _ := json.Marshal(c)
-		writer.Write(data)
-	})
-	if err := http.ListenAndServe(":8080", handler); err != nil {
-		log.Errorf("start config failed, the error is %v", err)
-	}
+	return c
 }
 
 type Configuration struct {
